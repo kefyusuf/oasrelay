2248975960e8a6af3f63b71c3705ece560cfff58
Yusuf KEF <41286150+kefyusuf@users.noreply.github.com>
2026-09-18 18:29:37 +0300

    test: use TLS for bearer runtime wiring

--- internal/mcpserver/bearer_env_test.go
+++ internal/mcpserver/bearer_env_test.go
@@ -13,11 +13,17 @@ import (
 
 func TestRuntimeHTTPClientReadsBearerTokenFromEnvironment(t *testing.T) {
 	authorization := make(chan string, 1)
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
 		authorization <- request.Header.Get("Authorization")
 		_, _ = io.WriteString(w, `{}`)
 	}))
 	defer server.Close()
+
+	originalDefaultTransport := http.DefaultTransport
+	http.DefaultTransport = server.Client().Transport
+	t.Cleanup(func() {
+		http.DefaultTransport = originalDefaultTransport
+	})
 
 	client, err := runtimeHTTPClient(func(name string) string {
 		if name != "OASRELAY_BEARER_TOKEN" {
